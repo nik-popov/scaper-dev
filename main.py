f78e4727550c6625a5178672257c273bae559091
Nik <149531476+nik-popov@users.noreply.github.com>
2025-10-10 20:43:45 +0000

    core

--- main.py
+++ main.py
@@ -786,7 +786,7 @@ async def generate_download_file(file_id: str, row_offset: int = 0):
             with open(local_filename, "wb") as f:
                 f.write(res.content)
             
-            header_row_value = header_row_from_db if header_row_from_db is not None else find_header_row_index(local_filename, logger_instance) or 0
+            header_row_value = header_row_from_db if header_row_from_db is not None else find_header_row_index(local_filename, logger_instance) or 1
             write_excel_generic(local_filename, temp_images_dir, header_row_value, row_offset, logger_instance)
         processed_file_name = f"{Path(file_name).stem}_processed_{timestamp}.xlsx"
         public_url = await upload_file_to_space(local_filename, save_as=f"processed_files/{processed_file_name}", file_id=file_id_int, is_public=True)
@@ -863,7 +863,7 @@ async def generate_msrp_excel(file_id: str, target_column: str, row_offset: int 
         with open(local_filename, "wb") as f:
             f.write(res.content)
         
-        header_row_value = header_row_from_db if header_row_from_db is not None else find_header_row_index(local_filename, logger_instance) or 1
+        header_row_value = header_row_from_db if header_row_from_db is not None else find_header_row_index(local_filename, logger_instance) or 0
         write_excel_msrp(local_filename, temp_images_dir, grouped_data, header_row_value, target_column, row_offset, logger_instance)
 
         processed_file_name = f"{Path(file_name).stem}_msrp_{timestamp}.xlsx"
